4678afc9198d5db95806881c8d71e1c8025ba445
Alexander Day <55056986+AD417@users.noreply.github.com>
2025-05-20 08:02:57 -0400

    Prevent crash on compilation error

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,22 @@
 module Main (main) where
 
 import Markdown.Tokens
-import Markdown.Nodes
 import Markdown.Parsing
 import Markdown.Assembly
 
 -- Determine the first complete and valid parse on the input string, and return
 -- the AST it produces. 
-firstCompleteParse :: String -> Node
-firstCompleteParse s =
+compile :: String -> String
+compile source =
     let
-        allParses = parseExpression $ tokenize s
-        (node, out) = head $ filter (\(_, arr) -> null arr) allParses
+        allParses = (parseExpression . tokenize) source
+        completeParses = filter (null . snd) allParses
     in
-    if null out
-    then node
-    else Text "*"
+    if not $ null completeParses then
+        (assemble . fst . head) completeParses
+    else
+        "Compilation Error"
 
 main :: IO ()
-main = print $ assemble $ firstCompleteParse "*italic and **bold* but maybe not?**"
-
+main = print $ compile "*italic and **bold* but maybe not?**"
+-- output: "<em>italic and <em>*bold</em> but maybe not?</em>*"
